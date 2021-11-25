6fd53d0d196bdbeef6fb2ec1e764528cfa314b6a
Raquel <80352388+Raquel296@users.noreply.github.com>
2021-11-25 18:19:37 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,22 +23,37 @@ public class Main {
 
             Libros libroprueba = new Libros();
             Libro libro1 = new Libro(1111,"El principito","Antoine de Saint-Exupéry","Éditions Gallimard",100,6000000);
+            Libro libro2 = new Libro(2222,"Reina Roja","Juan Gómez Jurado","Patatin",300,500000);
+            Libro libro3 = new Libro(333,"La villa de las telas","Anne Jacobs","Libritos",250,20000);
 
-            ArrayList listalibros = (ArrayList) libroprueba.verCatalogo();
-            listalibros.add(libro1);
 
             libroprueba.anadirLibro(libro1);
-            libroprueba.borrar(libro1);
-            libroprueba.obtenerLibro(1111);
-            libroprueba.librosporEditorial("Éditions Gallimard");
-            libroprueba.actualizarCopias(libro1);
+            libroprueba.anadirLibro(libro2);
+            libroprueba.anadirLibro(libro3);
 
+            //libroprueba.borrar(libro1);
+            //libroprueba.obtenerLibro(1111);
+            //libroprueba.librosporEditorial("Éditions Gallimard");
+            //libroprueba.actualizarCopias(libro1);
+
+            //Probando el ejercicio 1
+            System.out.println("Ejercicio 1 -->");
             System.out.println(Arrays.toString(libroprueba.getCamposLibro()));
+            libroprueba.verCatalogoInverso();
+
+            //Probando el ejercicio 2
+            System.out.println("Ejercicio 2 -->");
+            HashMap<Integer, Integer> actCopias = new HashMap<>();
+            actCopias.put(1111, 5000);
+            libroprueba.actualizarCopias(actCopias);
+
 
 
         } catch (AccesoDatosException e) {
             e.printStackTrace();
         }
     }
+
+
 
 }
